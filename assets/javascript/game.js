b952a733e316a1fea66d70fbe17470985f895961
RayKilburg <rkilburg42@gmail.com>
2018-06-18 02:59:05 -0400

    new

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+var wins = 0;
+var losses = 0;
+var rockOne;
+var rockTwo;
+var rockThree;
+var rockFour;
+var compScore;
+var userScore;
+
+//restart function
+//new game function
+//* The random number shown at the start of the game should be between 19 - 120.
+//function myFunction() {
+//document.getElementById("demo").innerHTML = Math.floor((Math.random() * 12) + 1);
+//function myFunction() {
+//document.getElementById("demo").innerHTML = Math.floor((Math.random() * 120) + 19);
+//* Each crystal should have a random hidden value between 1 - 12.
